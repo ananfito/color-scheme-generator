f37066da325ff6e9f17656a0d283c1b3aeaeb6cf
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-04-21 19:49:30 +0700

    completed project

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ const seedColorInput = document.getElementById('seed-color-input')
 const colorSchemeSelection = document.getElementById('color-scheme-select')
 const form = document.getElementById('color-selection-form')
 const colorSchemeResults = document.getElementById('color-scheme-results')
+let colorsHtml = ''
 
 // Event listener for button
 getColorSchemeBtn.addEventListener('click', getColorScheme)
+// Event listener for copy hex code to clipboard
+colorSchemeResults.addEventListener('click', copyToClipboard)
+
+// Copy hex code to clipboard
+function copyToClipboard(e) {
+    if (e.target.dataset) {
+        navigator.clipboard.writeText(e.target.dataset.hexValue)
+    }
+}
 
 // Get color scheme based on user input
 function getColorScheme(e) {
     e.preventDefault()
     
-    // remove # from seedColorInput.value
+    // Remove # from seedColorInput.value
     // convert string to array 
     let colorInputArr = seedColorInput.value.split('')
     // remove first element i=0 ('#') of the array
@@ -20,26 +30,22 @@ function getColorScheme(e) {
     // combined array to a new string & store as a new value for API
     const colorInputStr = colorInputArr.join('')
 
-    // fetch Color API data
+    // Fetch Color API data
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorInputStr}&mode=${colorSchemeSelection.value}&count=5`)
         .then(res => res.json())
         .then (data => {
-            // return array of colors
-            const colorsArr = data.colors
-
-            // loop through colorsArr and add hex value and image URL to colorSchemeResults
-            let html = ''
-            colorsArr.forEach(color => {
-                html += `
+            // loop through array of colors and add hex value and image URL to colorHtml
+            data.colors.forEach(color => {
+                colorsHtml += `
                     <div>
-                        <img src=${color.image.bare}>
-                        <p>${color.hex.value}</p>
+                        <img class="pointer" data-hex-value=${color.hex.value} src=${color.image.bare}>
+                        <p class="pointer" data-hex-value=${color.hex.value}>${color.hex.value}</p>
                     </div>
                 `
             });
-            colorSchemeResults.innerHTML = html
+            // return colorsHtml
+            colorSchemeResults.innerHTML = colorsHtml
+            document.getElementById('copy-instructions').style.display = 'block'
         })
-
-    form.reset() // NOTE: Are you sure you want to reset the form after the button has been clicked?
-    
+    colorsHtml = ''
 }
